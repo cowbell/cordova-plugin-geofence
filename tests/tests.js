459762c5dbd065615d240e212294b2061a8f2d18
Tomasz Subik <tsubik@gmail.com>
2014-10-30 12:34:12 +0100

    test for addOrUpdate geofence region

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,16 @@
 exports.defineAutoTests = function () {
+    var fail = function (done, reason) {
+            if (reason) {
+                console.log(reason);
+            }
+            expect(true).toBe(false);
+            done();
+        },
+        succeed = function (done) {
+            expect(true).toBe(true);
+            done();
+        };
+
     describe('Geofence plugin object', function () {
         it('should be defined in window object', function () {
             expect(window.geofence).toBeDefined();
@@ -27,6 +39,34 @@ exports.defineAutoTests = function () {
         it('should contain getWatched function', function () {
             expect(window.geofence.getWatched).toBeDefined();
             expect(typeof window.geofence.getWatched).toBe('function');
+        });
+    });
+
+    describe('addOrUpdate function', function () {
+        beforeEach(function () {
+
+        });
+
+        afterEach(function () {
+            window.geofence.removeAll();
+        });
+
+        it('should add single geofence', function (done) {
+            window.geofence
+                .addOrUpdate({
+                    id: '1',
+                    latitude: 50,
+                    longitude: 50,
+                    radius: 1000,
+                    transitionType: 1
+                })
+                .then(window.geofence.getWatched)
+                .then(function (geofencesJson) {
+                    var geofences = JSON.parse(geofencesJson);
+                    expect(geofences[0].id).toBe('1');
+                    done();
+                })
+                .catch(fail.bind(this, done));
         });
     });
 };
