0546f3f00a4aca7ae5884daac94cfac8b636044b
Tomasz Subik <tsubik@gmail.com>
2014-12-14 21:44:38 +0100

    swift support hook is now plugin hook

--- hooks/after_plugin_install/ios_swift_support.js
+++ hooks/after_plugin_install/ios_swift_support.js
@@ -10,6 +10,8 @@ var fs = require("fs"),
     glob = require("glob"),
     COMMENT_KEY = /_comment$/,
     projectRoot = process.argv[2];
+//if run for plugin projectRoot initialy is platform
+projectRoot = path.join(projectRoot,'..');
 
 run(projectRoot);
 
@@ -40,8 +42,10 @@ function run(projectRoot) {
             shell.echo('An error occured during parsing of [' + xcodeProjectPath + ']: ' + JSON.stringify(err));
         } else {
             bridgingHeaderPath = getBridgingHeader(xcodeProject);
-            if(!bridgingHeaderPath) {
-                bridgingHeaderPath = createBridgingHeader(xcodeProject, iosProjectFilesPath);
+            if(bridgingHeaderPath) {
+                bridgingHeaderPath = path.join(iosPlatformPath, bridgingHeaderPath);
+            } else {
+                bridgingHeaderPath = createBridgingHeader(xcodeProject, projectName, iosProjectFilesPath);
             }
             importBridgingHeaders(bridgingHeaderPath, getPluginsBridgingHeaders(iosProjectFilesPath));
             var configurations = nonComments(xcodeProject.pbxXCBuildConfigurationSection()),
@@ -55,6 +59,8 @@ function run(projectRoot) {
             shell.echo('[' + xcodeProjectPath + '] now has deployment target set as:[' + IOS_DEPLOYMENT_TARGET + '] ...');
             shell.echo('[' + xcodeProjectPath + '] option EMBEDDED_CONTENT_CONTAINS_SWIFT set as:[YES] ...');
             shell.echo('[' + xcodeProjectPath + '] swift_objc Bridging-Header set to:[' + bridgingHeaderPath + '] ...');
+            shell.echo('[' + xcodeProjectPath + '] Adding libsqlite3...');
+            xcodeProject.addFramework("libsqlite3.dylib");
             fs.writeFileSync(xcodeProjectPath, xcodeProject.writeSync(), 'utf-8');
         }
     });
@@ -73,17 +79,18 @@ function getBridgingHeader(xcodeProject) {
     }
 }
 
-function createBridgingHeader(xcodeProject, xcodeProjectRootPath) {
-    var newBHPath = path.join(xcodeProjectRootPath, "Classes", "Bridging-Header.h"),
+function createBridgingHeader(xcodeProject, projectName, xcodeProjectRootPath) {
+    var newBHPath = path.join(xcodeProjectRootPath, "Plugins", "Bridging-Header.h"),
         content = ["//",
         "//  Use this file to import your target's public headers that you would like to expose to Swift.",
         "//",
-        "#import<Cordova/CDV.h>"]
+        "#import <Cordova/CDV.h>"]
 
     //fs.openSync(newBHPath, 'w');
     shell.echo('Creating new Bridging-Header.h at path: ', newBHPath);
     fs.writeFileSync(newBHPath, content.join("\n"), { encoding: 'utf-8', flag: 'w' });
-    xcodeProject.addSourceFile(newBHPath);
+    xcodeProject.addHeaderFile("Bridging-Header.h");
+    setBridgingHeader(xcodeProject, path.join(projectName, "Plugins", "Bridging-Header.h"));
     return newBHPath;
 }
 
@@ -93,13 +100,13 @@ function setBridgingHeader(xcodeProject, headerPath) {
 
     for (config in configurations) {
         buildSettings = configurations[config].buildSettings;
-        buildSettings['SWIFT_OBJC_BRIDGING_HEADER'] = headerPathś;
+        buildSettings['SWIFT_OBJC_BRIDGING_HEADER'] = headerPath;
     }
 }
 
 function getPluginsBridgingHeaders(xcodeProjectRootPath) {
     var searchPath = path.join(xcodeProjectRootPath, 'Plugins');
-    console.log(searchPath);
+
     return glob.sync("**/*Bridging-Header*.h", { cwd: searchPath })
         .map(function(filePath) {
             return path.basename(path.join(searchPath, filePath));
@@ -107,14 +114,15 @@ function getPluginsBridgingHeaders(xcodeProjectRootPath) {
 }
 
 function importBridgingHeaders(mainBridgingHeader, headers) {
-    var content = fs.readFileSync(mainBridgingHeader, 'utf-8');
-    console.log('Headers', headers);
+    var content = fs.readFileSync(mainBridgingHeader, 'utf-8'),
+        mainHeaderName = path.basename(mainBridgingHeader);
+
     headers.forEach(function (header) {
-        if(content.indexOf(header) < 0) {
+        if(header !== mainHeaderName && content.indexOf(header) < 0) {
             if (content.charAt(content.length - 1) != '\n') {
                 content += "\n";
             }
-            content += "#import<"+header+">\n"
+            content += "#import \""+header+"\"\n"
             shell.echo('Importing ' + header + ' into main bridging-header at: ' + mainBridgingHeader);
         }
     });
@@ -126,11 +134,8 @@ function getProjectName(protoPath) {
         xmlObject = xml.parseFileSync(cordovaConfigPath)
         //content = fs.readFileSync(cordovaConfigPath, 'utf-8'),
         //use better lib
+
     return xmlObject.childs[0].childs[0]
-}
-
-function addHeaderFile(xcodeProject, file) {
-
 }
 
 function nonComments(obj) {
