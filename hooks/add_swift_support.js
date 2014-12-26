6aa0db75e3ab98ccc7a367c5f2dd5a3073fadbe0
Tomasz Subik <tsubik@gmail.com>
2014-12-26 15:46:09 +0100

    new swift support hook using cached xcode project from cordova-lib

--- hooks/add_swift_support.js
+++ hooks/add_swift_support.js
@@ -1,73 +1,55 @@
-#!/usr/bin/env node
+var fs = require('fs'),
+    path = require('path'),
+    shell = require('shelljs'),
+    glob = require('glob');
 
 module.exports = function(context) {
 
-    var IOS_DEPLOYMENT_TARGET = '7.0';
-
-    var fs = require("fs"),
-        path = require("path"),
-        shell = require("shelljs"),
-        xcode = require('xcode'),
-        xml = require("node-xml-lite"),
-        glob = require("glob"),
-        readline = require("readline"),
+    var IOS_DEPLOYMENT_TARGET = '7.0',
         COMMENT_KEY = /_comment$/,
         projectRoot = process.argv[2];
+
     //if run for plugin projectRoot initialy is platform
     projectRoot = path.join(projectRoot,'..');
 
     run(projectRoot);
-    /*
-      This is our runner function. It sets up the project paths,
-      parses the project file using xcode and delegates to our updateDeploymentTarget
-      that does the actual work.
-    */
 
     function run(projectRoot) {
-        var projectName = getProjectName(projectRoot),
-            xcodeProjectName = projectName + '.xcodeproj',
+        var cordova_util = context.requireCordovaModule('cordova-lib/src/cordova/util'),
+            ConfigParser = context.requireCordovaModule('cordova-lib/src/configparser/ConfigParser'),
+            platforms = context.requireCordovaModule('cordova-lib/src/plugman/platforms'),
+            xml = cordova_util.projectConfig(projectRoot),
+            cfg = new ConfigParser(xml),
+            projectName = cfg.name(),
             iosPlatformPath = path.join(projectRoot, 'platforms', 'ios'),
             iosProjectFilesPath = path.join(iosPlatformPath, projectName),
-            xcodeProjectPath = path.join(iosPlatformPath, xcodeProjectName, 'project.pbxproj'),
+            xcodeProject = platforms['ios'].parseProjectFile(iosPlatformPath).xcode,
             bridgingHeaderPath;
 
-        if (!fs.existsSync(xcodeProjectPath)) {
-            return;
+        shell.echo('Adjusting iOS deployment target for ' + projectName + ' to: [' + IOS_DEPLOYMENT_TARGET + '] ...');
+
+        bridgingHeaderPath = getBridgingHeader(xcodeProject);
+        if(bridgingHeaderPath) {
+            bridgingHeaderPath = path.join(iosPlatformPath, bridgingHeaderPath);
+        } else {
+            bridgingHeaderPath = createBridgingHeader(xcodeProject, projectName, iosProjectFilesPath);
         }
+        importBridgingHeaders(bridgingHeaderPath, getPluginsBridgingHeaders(iosProjectFilesPath));
+        var configurations = nonComments(xcodeProject.pbxXCBuildConfigurationSection()),
+            config, buildSettings;
 
-        xcodeProject = xcode.project(xcodeProjectPath);
-
-        shell.echo("Adjusting iOS deployment target for " + projectName + " to: [" + IOS_DEPLOYMENT_TARGET + "] ...");
-
-        xcodeProject.parse(function(err) {
-            if (err) {
-                shell.echo('An error occured during parsing of [' + xcodeProjectPath + ']: ' + JSON.stringify(err));
-            } else {
-                bridgingHeaderPath = getBridgingHeader(xcodeProject);
-                if(bridgingHeaderPath) {
-                    bridgingHeaderPath = path.join(iosPlatformPath, bridgingHeaderPath);
-                } else {
-                    bridgingHeaderPath = createBridgingHeader(xcodeProject, projectName, iosProjectFilesPath);
-                }
-                importBridgingHeaders(bridgingHeaderPath, getPluginsBridgingHeaders(iosProjectFilesPath));
-                var configurations = nonComments(xcodeProject.pbxXCBuildConfigurationSection()),
-                    config, buildSettings;
-
-                for (config in configurations) {
-                    buildSettings = configurations[config].buildSettings;
-                    buildSettings['IPHONEOS_DEPLOYMENT_TARGET'] = IOS_DEPLOYMENT_TARGET;
-                    buildSettings['EMBEDDED_CONTENT_CONTAINS_SWIFT'] = "YES";
-                    buildSettings['LD_RUNPATH_SEARCH_PATHS'] = '"@executable_path/Frameworks"'
-                }
-                shell.echo('[' + xcodeProjectPath + '] now has deployment target set as:[' + IOS_DEPLOYMENT_TARGET + '] ...');
-                shell.echo('[' + xcodeProjectPath + '] option EMBEDDED_CONTENT_CONTAINS_SWIFT set as:[YES] ...');
-                shell.echo('[' + xcodeProjectPath + '] swift_objc Bridging-Header set to:[' + bridgingHeaderPath + '] ...');
-                shell.echo('[' + xcodeProjectPath + '] Runpath Search Paths set to: @executable_path/Frameworks ...');
-                shell.echo('[' + xcodeProjectPath + '] Adding libsqlite3...');
-                xcodeProject.addFramework("libsqlite3.dylib");
-                fs.writeFileSync(xcodeProjectPath, xcodeProject.writeSync(), 'utf-8');
-            }
-        });
+        for (config in configurations) {
+            buildSettings = configurations[config].buildSettings;
+            buildSettings['IPHONEOS_DEPLOYMENT_TARGET'] = IOS_DEPLOYMENT_TARGET;
+            buildSettings['EMBEDDED_CONTENT_CONTAINS_SWIFT'] = "YES";
+            buildSettings['LD_RUNPATH_SEARCH_PATHS'] = '"@executable_path/Frameworks"'
+        }
+        shell.echo('IOS project now has deployment target set as:[' + IOS_DEPLOYMENT_TARGET + '] ...');
+        shell.echo('IOS project option EMBEDDED_CONTENT_CONTAINS_SWIFT set as:[YES] ...');
+        shell.echo('IOS project swift_objc Bridging-Header set to:[' + bridgingHeaderPath + '] ...');
+        shell.echo('IOS project Runpath Search Paths set to: @executable_path/Frameworks ...');
+        shell.echo('IOS project Adding libsqlite3...');
+        xcodeProject.addFramework("libsqlite3.dylib");
     }
 
     function getBridgingHeader(xcodeProject) {
@@ -131,15 +113,6 @@ module.exports = function(context) {
             }
         });
         fs.writeFileSync(mainBridgingHeader, content, 'utf-8');
-    }
-
-    function getProjectName(protoPath) {
-        var cordovaConfigPath = path.join(protoPath, 'config.xml'),
-            xmlObject = xml.parseFileSync(cordovaConfigPath)
-            //content = fs.readFileSync(cordovaConfigPath, 'utf-8'),
-            //use better lib
-
-        return xmlObject.childs[0].childs[0]
     }
 
     function nonComments(obj) {
