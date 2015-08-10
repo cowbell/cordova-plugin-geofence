4068960d1bba635a4ae437e2e877aa47e21d7dbe
Andres Cartin <skullfirec@gmail.com>
2015-08-10 11:45:33 -0600

    Changed promise namespace

--- www/geofence.js
+++ www/geofence.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec'),
-    Promise = require('com.vladstirbu.cordova.promise.Promise'),
+    Promise = require('es6-promise-plugin.Promise'),
     channel = require('cordova/channel'),
     geofence,
     Geofence = function () {
