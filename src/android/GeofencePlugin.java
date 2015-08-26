8bdb5d6af3e009d72cbe1726fd818bb7bf344469
Tomasz Subik <tsubik@gmail.com>
2015-08-26 13:41:42 +0200

    missing callback context

--- src/android/GeofencePlugin.java
+++ src/android/GeofencePlugin.java
@@ -67,14 +67,13 @@ public class GeofencePlugin extends CordovaPlugin {
             callbackContext.success(Gson.get().toJson(geoNotifications));
         } else if (action.equals("initialize")) {
             callbackContext.success();
-
         } else if (action.equals("deviceReady")) {
             deviceReady();
+            callbackContext.success();
         } else {
             return false;
         }
         return true;
-
     }
 
     private GeoNotification parseFromJSONObject(JSONObject object) {
