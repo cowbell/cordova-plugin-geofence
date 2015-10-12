b525f43176d30d97b69ec054a235e5610f37df57
Tomasz Subik <tsubik@gmail.com>
2015-10-12 18:49:29 +0200

    addOrUpdate geofences coercion, closes #84

--- www/geofence.js
+++ www/geofence.js
@@ -1,5 +1,5 @@
-var exec = require('cordova/exec'),
-    channel = require('cordova/channel');
+var exec = require("cordova/exec"),
+    channel = require("cordova/channel");
 
 module.exports = {
     /**
@@ -12,7 +12,7 @@ module.exports = {
      * @return {Promise}
      */
     initialize: function (success, error) {
-        return execPromise(success, error, 'GeofencePlugin', 'initialize', []);
+        return execPromise(success, error, "GeofencePlugin", "initialize", []);
     },
     /**
      * Adding new geofence to monitor.
@@ -29,7 +29,10 @@ module.exports = {
         if (!Array.isArray(geofences)) {
             geofences = [geofences];
         }
-        return execPromise(success, error, 'GeofencePlugin', 'addOrUpdate', geofences);
+
+        geofences.forEach(coerceProperties);
+
+        return execPromise(success, error, "GeofencePlugin", "addOrUpdate", geofences);
     },
     /**
      * Removing geofences with given ids
@@ -44,7 +47,7 @@ module.exports = {
         if (!Array.isArray(ids)) {
             ids = [ids];
         }
-        return execPromise(success, error, 'GeofencePlugin', 'remove', ids);
+        return execPromise(success, error, "GeofencePlugin", "remove", ids);
     },
     /**
      * removing all stored geofences on the device
@@ -55,7 +58,7 @@ module.exports = {
      * @return {Promise}
      */
     removeAll: function (success, error) {
-        return execPromise(success, error, 'GeofencePlugin', 'removeAll', []);
+        return execPromise(success, error, "GeofencePlugin", "removeAll", []);
     },
     /**
      * Getting all watched geofences from the device
@@ -66,7 +69,7 @@ module.exports = {
      * @return {Promise} if successful returns geofences array stringify to JSON
      */
     getWatched: function (success, error) {
-        return execPromise(success, error, 'GeofencePlugin', 'getWatched', []);
+        return execPromise(success, error, "GeofencePlugin", "getWatched", []);
     },
     /**
      * Called when app is opened via Notification bar
@@ -96,7 +99,7 @@ module.exports = {
      * @return {Promise}
      */
     ping: function (success, error) {
-        return execPromise(success, error, 'GeofencePlugin', 'ping', []);
+        return execPromise(success, error, "GeofencePlugin", "ping", []);
     }
 };
 
@@ -104,13 +107,13 @@ function execPromise(success, error, pluginName, method, args) {
     return new Promise(function (resolve, reject) {
         exec(function (result) {
                 resolve(result);
-                if (typeof success === 'function') {
+                if (typeof success === "function") {
                     success(result);
                 }
             },
             function (reason) {
                 reject(reason);
-                if (typeof error === 'function') {
+                if (typeof error === "function") {
                     error(reason);
                 }
             },
@@ -120,9 +123,112 @@ function execPromise(success, error, pluginName, method, args) {
     });
 }
 
-// Called after 'deviceready' event
+function coerceProperties(geofence) {
+    if (geofence.id) {
+        geofence.id = geofence.id.toString();
+    } else {
+        throw new Error("Geofence id is not provided");
+    }
+
+    if (geofence.latitude) {
+        geofence.latitude = coerceNumber("Geofence latitude", geofence.latitude);
+    } else {
+        throw new Error("Geofence latitude is not provided");
+    }
+
+    if (geofence.longitude) {
+        geofence.longitude = coerceNumber("Geofence longitude", geofence.longitude);
+    } else {
+        throw new Error("Geofence longitude is not provided");
+    }
+
+    if (geofence.radius) {
+        geofence.radius = coerceNumber("Geofence radius", geofence.radius);
+    } else {
+        throw new Error("Geofence radius is not provided");
+    }
+
+    if (geofence.transitionType) {
+        geofence.transitionType = coerceNumber("Geofence transitionType", geofence.transitionType);
+    } else {
+        throw new Error("Geofence transitionType is not provided");
+    }
+
+    if (geofence.notification) {
+        if (geofence.notification.id) {
+            geofence.notification.id = coerceNumber("Geofence notification.id", geofence.notification.id);
+        }
+
+        if (geofence.notification.title) {
+            geofence.notification.title = geofence.notification.title.toString();
+        }
+
+        if (geofence.notification.text) {
+            geofence.notification.text = geofence.notification.text.toString();
+        }
+
+        if (geofence.notification.smallIcon) {
+            geofence.notification.smallIcon = geofence.notification.smallIcon.toString();
+        }
+
+        if (geofence.notification.openAppOnClick) {
+            geofence.notification.openAppOnClick = coerceBoolean("Geofence notification.openAppOnClick", geofence.notification.openAppOnClick);
+        }
+
+        if (geofence.notification.vibration) {
+            if (Array.isArray(geofence.notification.vibration)) {
+                for (var i=0; i<geofence.notification.vibration.length; i++) {
+                    geofence.notification.vibration[i] = coerceInteger("Geofence notification.vibration["+ i +"]", geofence.notification.vibration[i]);
+                }
+            } else {
+                throw new Error("Geofence notification.vibration is not an Array");
+            }
+        }
+    }
+}
+
+function coerceNumber(name, value) {
+    if (typeof(value) !== "number") {
+        console.warn(name + " is not a number, trying to convert to number");
+        value = Number(value);
+
+        if (isNaN(value)) {
+            throw new Error("Cannot convert " + name + " to number");
+        }
+    }
+
+    return value;
+}
+
+function coerceInteger(name, value) {
+    if (!isInt(value)) {
+        console.warn(name + " is not an integer, trying to convert to integer");
+        value = parseInt(value);
+
+        if (isNaN(value)) {
+            throw new Error("Cannot convert " + name + " to integer");
+        }
+    }
+
+    return value;
+}
+
+function coerceBoolean(name, value) {
+    if (typeof(value) !== "boolean") {
+        console.warn(name + " is not a boolean value, converting to boolean");
+        value = Boolean(value);
+    }
+
+    return value;
+}
+
+function isInt(n){
+    return Number(n) === n && n % 1 === 0;
+}
+
+// Called after "deviceready" event
 channel.deviceready.subscribe(function () {
     // Device is ready now, the listeners are registered
     // and all queued events can be executed.
-    exec(null, null, 'GeofencePlugin', 'deviceReady', []);
+    exec(null, null, "GeofencePlugin", "deviceReady", []);
 });
