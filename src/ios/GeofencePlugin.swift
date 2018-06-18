76d54c67a4c7dfc36d124c62a44d8406851fe8a8
Jordan Schneider <t-josch@microsoft.com>
2018-06-18 15:27:27 -0700

    Fixed final swift 4 build issues.

--- src/ios/GeofencePlugin.swift
+++ src/ios/GeofencePlugin.swift
@@ -140,7 +140,7 @@ func log(_ messages: [String]) {
         }
     }
 
-    func didReceiveTransition (_ notification: Notification) {
+    @objc func didReceiveTransition (_ notification: Notification) {
         log("didReceiveTransition")
         if let geoNotificationString = notification.object as? String {
 
@@ -150,7 +150,7 @@ func log(_ messages: [String]) {
         }
     }
 
-    func didReceiveLocalNotification (_ notification: Notification) {
+    @objc func didReceiveLocalNotification (_ notification: Notification) {
         log("didReceiveLocalNotification")
         if UIApplication.shared.applicationState != UIApplicationState.active {
             var data = "undefined"
